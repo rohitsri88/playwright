d1e42537a503bd6115ad75c4a8be52640c528b4c
ht-rohit <rohit@haventech.asia>
2023-01-26 13:16:05 +0800

    first commit to this project

---
+++ tests/multiplePackages.spec.ts
@@ -0,0 +1,16 @@
+import endpoint from "./configTypes"
+import { test, expect } from '@playwright/test'
+
+test("Expect to have 3 packages for subscription", async ({ page }) => {
+
+  // Go to the Droplets product page of DigitalOcean web page
+  await page.goto(endpoint.DIGITAL_OCEAN_URL);
+
+  // Wait for the page to load
+  await page.waitForLoadState('networkidle');
+
+  // Get the number of packages to be 2 (Basic and Premium)
+  const number_subscriptions_allowed = await page.locator('.CPUInfoStyles__StyledLeftCpuInfo-sc-ooo7a2-4 > div').count()
+  // Verify that number equals 2
+  expect(number_subscriptions_allowed).toBe(1)
+});
